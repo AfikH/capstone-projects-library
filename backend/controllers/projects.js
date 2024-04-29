658b893dbec200d3cc5c86960065ccfb125a70b3
Afik Habaz <afikhabaz@gmail.com>
2024-04-29 15:48:44 +0300

    Backend -> Adding insert/getAll/getOne/Delete to projects controller

--- backend/controllers/projects.js
+++ backend/controllers/projects.js
@@ -1,6 +1,21 @@
 import Db from "../lib/db.js";
 
-const getAllProjects = async (cb) => {
+export const insertOne = async (project) => {
+    let db = new Db();
+
+    try{
+        await db.createConnection();
+        await db.prepare('INSERT INTO projects (title, content, project_author) VALUES (?,?,?)');
+        let result = await db.execute([project.title, project.content, project.projectAuthor]);
+
+        return result.insertId;
+    }catch(error){
+        console.log(error);
+        throw new Error();
+    }
+}
+
+export const getAll = async () => {
     let db = new Db();
 
     try{
@@ -8,9 +23,39 @@ const getAllProjects = async (cb) => {
         await db.prepare('SELECT * FROM projects');
         let result = await db.execute();
 
-        return cb(false, result);
+        return result;
     }catch(error){
         console.log(error);
-        return cb({ msg: 'Error fetching projects table.' }, result);
+        throw new Error();
+    }
+}
+
+export const getOneById = async (id) => {
+    let db = new Db();
+
+    try{
+        await db.createConnection();
+        await db.prepare('SELECT * FROM projects WHERE project_id=? LIMIT 1');
+        let result = await db.execute([parseInt(id)]);
+
+        return result[0];
+    }catch(error){
+        console.log(error);
+        throw new Error();
+    }
+}
+
+export const deleteOne = async (id) => {
+    let db = new Db();
+
+    try{
+        await db.createConnection();
+        await db.prepare('DELETE FROM projects WHERE project_id=? LIMIT 1');
+        let result = await db.execute([parseInt(id)]);
+
+        return result.affectedRows;
+    }catch(error){
+        console.log(error);
+        throw new Error();
     }
 }
